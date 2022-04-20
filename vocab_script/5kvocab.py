ff2f3ef195ca342faf2393ccae10ea4cf0cbcc56
CasCodes <64489325+CasCodes@users.noreply.github.com>
2022-04-20 19:10:20 +0200

    added top5k vocab script

---
+++ vocab_script/5kvocab.py
@@ -0,0 +1,84 @@
+import sys
+import numpy as np
+import pandas as pd
+import re
+from collections import Counter
+from nltk.corpus import stopwords
+import glob
+import os
+
+
+# --- DEFINE PIPELINE FUNCTIONS ---
+
+custom_stopwords = ["semst", "im"]
+# takes in string & returns a cleaned string of all non-stop-words
+def preprocess(text):
+    sw = stopwords.words('english')
+    text = re.sub(r'[^\w\s]', '', text).lower()
+    s = ""
+    for word in text.split():
+        if word not in sw and word not in custom_stopwords:
+                s += (word + " ")
+    return s
+
+# Takes array of dataframes, returns df with top5k dictionary
+def multidf_vocab(df_arr):
+    # create array of cleaned strings
+    vocab = []
+    for df in df_arr:
+        for i in range(len(df)):
+            # This has the 2nd column hardcoded!! Change it for production
+            vocab.append(preprocess(df[2][i]))
+    vocab_df = pd.DataFrame(vocab)
+    # how do I use counter without turning the vocab array into a df first?
+    # count appearance of each word & create frequency dataframe
+    return vocab_df
+
+
+# takes in cleaned text df, return top 5k frequency df
+def tf5k(processed_df):
+    counter = Counter(" ".join(processed_df[0]).split()).most_common(5000)
+    counter_df = pd.DataFrame(counter)
+    return counter_df
+
+# try reading each df
+def read_dfs(directory):
+    dfs = []
+    try:
+        for filename in directory: 
+            df = pd.read_csv(filename, sep="\t", header=None) 
+            dfs.append(df)
+            print("dataframes read...")
+            return dfs
+    except:
+        print("reading dataframes failed\nquitting...")
+        sys.exit()
+  
+
+# call all of the pipeline functions
+def pipeline(dfs):
+    # --PIPELINE---
+    try:
+        data = multidf_vocab(dfs)
+        top5k = tf5k(data)
+        
+        # save to file
+        top5k.to_csv('./vocab.csv')
+
+        print("created top 5k dictionary")
+    except:
+        print("pipeline error!\nquitting...")
+        sys.exit()
+
+# --- START ---
+
+# catch wrong usage
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print("usage:\n python3 5kvocab.py <folder>")
+else:
+    path = sys.argv[1]
+    directory = glob.glob(os.path.join(path , "*.txt")) 
+    print("dir:", directory)
+    
+    dfs = read_dfs(directory)
+    pipeline(dfs)
---
+++ vocab_script/README.md
@@ -0,0 +1,16 @@
+# helpers
+This repo contains all of the scripts that make your life easier!
+
+## top5k.py
+usage:
+`python3 top5k.py <folder>`
+
+> The folder should contain only datasets
+
+The script will read every data frame in the folder and write a
+csv with the top 5k most common words of all data frames combined
+to the current directory.
+
+IMPORTANT:
+> Note that the row at which it looks for is currently hard-coded to 2.
+> Furthermore, it only look for .txt files in the directory!
